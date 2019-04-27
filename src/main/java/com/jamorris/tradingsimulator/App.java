1ce7d065e6a34d71a3258991c7cb84d22effc55b
Jacob <jamorris1819@gmail.com>
2019-04-27 13:44:33 +0100

    Created solution

---
+++ src/main/java/com/jamorris/tradingsimulator/App.java
@@ -0,0 +1,13 @@
+package com.jamorris.tradingsimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
